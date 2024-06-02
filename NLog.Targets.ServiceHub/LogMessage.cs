a042a7c269ad2e9c95db773ac3a6d31e487b4378
Brian Griggs <brian@bigmissionmotorsports.com>
2024-06-01 18:13:55 -0600

    Updating to net8

--- NLog.Targets.ServiceHub/LogMessage.cs
+++ NLog.Targets.ServiceHub/LogMessage.cs
@@ -1,12 +1,11 @@
 ﻿using Newtonsoft.Json;
 
-namespace NLog.Targets.ServiceHub
+namespace BigMission.NLog.Targets.ServiceHub;
+
+public class LogMessage
 {
-    public class LogMessage
-    {
-        [JsonProperty("sk")]
-        public Guid SourceKey { get; set; }
-        [JsonProperty("m")]
-        public string? Message { get; set; }
-    }
+    [JsonProperty("sk")]
+    public Guid SourceKey { get; set; }
+    [JsonProperty("m")]
+    public string? Message { get; set; }
 }
--- NLog.Targets.ServiceHub/ServiceHubTarget.cs
+++ NLog.Targets.ServiceHub/ServiceHubTarget.cs
@@ -1,76 +1,80 @@
 ﻿#pragma warning disable 8618
 using BigMission.CommandTools;
 using Microsoft.AspNetCore.SignalR.Client;
+using NLog;
 using NLog.Config;
 using NLog.Layouts;
+using NLog.Targets;
 
-namespace NLog.Targets.ServiceHub
+namespace BigMission.NLog.Targets.ServiceHub;
+
+[Target("ServiceHub")]
+public class ServiceHubTarget : TargetWithLayout
 {
-    [Target("ServiceHub")]
-    public class ServiceHubTarget : TargetWithLayout
+    [RequiredParameter]
+    public Layout SourceKey { get; set; }
+    [RequiredParameter]
+    public Layout HubUrl { get; set; }
+    [RequiredParameter]
+    public Layout ApiKey { get; set; }
+    [RequiredParameter]
+    public Layout AesKey { get; set; }
+
+    private HubConnection hubConnection;
+
+
+    protected override void InitializeTarget()
     {
-        [RequiredParameter]
-        public Layout SourceKey { get; set; }
-        [RequiredParameter]
-        public Layout HubUrl { get; set; }
-        [RequiredParameter]
-        public Layout ApiKey { get; set; }
-
-        private HubConnection hubConnection;
-
-
-        protected override void InitializeTarget()
+        var deviceSourceStr = SourceKey?.Render(LogEventInfo.CreateNullEvent());
+            var url = HubUrl?.Render(LogEventInfo.CreateNullEvent());
+        if (Guid.TryParse(deviceSourceStr, out Guid deviceSourceKey) && !string.IsNullOrWhiteSpace(url))
         {
-            var deviceSourceStr = SourceKey?.Render(LogEventInfo.CreateNullEvent());
-                var url = HubUrl?.Render(LogEventInfo.CreateNullEvent());
-            if (Guid.TryParse(deviceSourceStr, out Guid deviceSourceKey) && !string.IsNullOrWhiteSpace(url))
-            {
-                var apiKey = ApiKey?.Render(LogEventInfo.CreateNullEvent());
+            var apiKey = ApiKey?.Render(LogEventInfo.CreateNullEvent());
+            var aesKey = AesKey?.Render(LogEventInfo.CreateNullEvent());
 
-                hubConnection = new HubConnectionBuilder()
-                    .WithUrl(url, option =>
-                    {
-                        option.AccessTokenProvider = async () =>
-                        {
-                            var token = KeyUtilities.EncodeToken(deviceSourceKey, apiKey);
-                            return await Task.FromResult(token);
-                        };
-                    }).Build();
-
-                base.InitializeTarget();
-            }
-            else
-            {
-                throw new InvalidOperationException();
-            }
-        }
-
-        protected override void CloseTarget()
-        {
-            hubConnection.DisposeAsync();
-
-            base.CloseTarget();
-        }
-
-        protected override void Write(LogEventInfo logEvent)
-        {
-            var message = Layout.Render(logEvent);
-            var sourceKey = SourceKey.Render(logEvent);
-            var lm = new LogMessage { Message = message, SourceKey = new Guid(sourceKey) };
-
-            if (hubConnection.State == HubConnectionState.Disconnected && hubConnection.State != HubConnectionState.Connecting)
-            {
-                try
+            hubConnection = new HubConnectionBuilder()
+                .WithUrl(url, option =>
                 {
-                    hubConnection.StartAsync();
-                }
-                catch { }
-            }
+                    option.AccessTokenProvider = async () =>
+                    {
+                        var token = KeyUtilities.EncodeToken(deviceSourceKey, apiKey, aesKey);
+                        return await Task.FromResult(token);
+                    };
+                }).Build();
 
-            if (hubConnection.State == HubConnectionState.Connected)
+            base.InitializeTarget();
+        }
+        else
+        {
+            throw new InvalidOperationException();
+        }
+    }
+
+    protected override async void CloseTarget()
+    {
+        await hubConnection.DisposeAsync();
+
+        base.CloseTarget();
+    }
+
+    protected override async void Write(LogEventInfo logEvent)
+    {
+        var message = Layout.Render(logEvent);
+        var sourceKey = SourceKey.Render(logEvent);
+        var lm = new LogMessage { Message = message, SourceKey = new Guid(sourceKey) };
+
+        if (hubConnection.State == HubConnectionState.Disconnected && hubConnection.State != HubConnectionState.Connecting)
+        {
+            try
             {
-                hubConnection.SendAsync("PostLogMessage", lm).Wait();
+                await hubConnection.StartAsync();
             }
+            catch { }
+        }
+
+        if (hubConnection.State == HubConnectionState.Connected)
+        {
+            await hubConnection.SendAsync("PostLogMessage", lm);
         }
     }
 }
